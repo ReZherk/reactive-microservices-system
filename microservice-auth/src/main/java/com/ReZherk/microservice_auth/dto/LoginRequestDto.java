5986ec7f9999281ccb3cb7b0782f41b2af2d0190
ReZherk <patrickcomeresbueno@gmail.com>
2025-12-11 16:56:17 -0500

    feat(auth):Create dto and entity for user login

---
+++ microservice-auth/src/main/java/com/ReZherk/microservice_auth/dto/LoginRequestDto.java
@@ -0,0 +1,13 @@
+package com.ReZherk.microservice_auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDto {
+    private String username;
+    private String password;
+}
---
+++ microservice-auth/src/main/java/com/ReZherk/microservice_auth/dto/RegisterRequestDto.java
@@ -0,0 +1,15 @@
+package com.ReZherk.microservice_auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequestDto {
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+}
